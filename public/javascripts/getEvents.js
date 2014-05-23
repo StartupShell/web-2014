f85c92b25a26b62c60a7290b1ffe50ced20e01e1
jacksongeller <jacksongeller@gmail.com>
2014-05-23 17:22:55 -0400

    Date is refactored.

--- public/javascripts/getEvents.js
+++ public/javascripts/getEvents.js
@@ -12,33 +12,33 @@
   })
   .done(function(data) {
 
-    console.log(data);
-
     for (var i = 0; i <data.feed.entry.length; i++) {
       
       function getStandTime(time) {
         if (time> 12) {
-          return time - 12 + ' PM'
+          return time - 12
         }
         else if (time === 0) {
           return ""
         }
         else if (time < 12 && time > 0) {
-          return time + ' AM'
+          return time
         }
-        
+      }
+      function convertToDateString(date) {
+        var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+        var stand = 'AM'
+        if (date.getUTCHours() > 12) stand = 'PM'
+        var str = months[date.getUTCMonth()] + ' ' + date.getUTCDate() + ', ' + getStandTime(date.getUTCHours()) + ':' + ('0' + date.getUTCMinutes()).slice(-2) + ' ' + stand
+        return str
       }
 
       var when = data.feed.entry[i].gd$when[0].startTime;
       var date = Date.parse(when.split('.')[0]);      
       date = new Date(date);
       
-      var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-      var month = months[date.getUTCMonth()]
-      var day = date.getUTCDate()
-      var time = getStandTime(date.getUTCHours())
-      var dateString = month + ' ' + day + ', ' + time
-
+      var dateString = convertToDateString(date)
+      
       var html ='<a href='+data.feed.entry[i].link[0].href+'>' + 
                 '<h3>' +data.feed.entry[i].title.$t + '</h3>'+
                 '<p><span class="startTime">'+dateString+'</span> @ '+
