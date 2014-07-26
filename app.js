9eb71e176e68f6c2782f4418c02c927c2cd1da9f
jacksongeller <jacksongeller@gmail.com>
2014-07-26 19:56:01 -0400

    remove logging

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(logger());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 86400000 } ));
