b390d84c936e63a6e0af7d1ae5f8ab01920b59b7
Chris Szeluga <cs850x@aol.com>
2014-10-08 20:07:28 -0400

    scroll

--- public/main.js
+++ public/main.js
@@ -37,6 +37,22 @@ window.onresize = function(event) {
     resizeDiv();
 }
 
+//SCROLL
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').stop().animate({
+          scrollTop: target.offset().top-60
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 
 //GOOGLE EVENTS
 jQuery(function(){
