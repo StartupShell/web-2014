8e5853f50ad3cfb98fd6aa595078dc874656b31b
jacksongeller <jacksongeller@gmail.com>
2014-10-10 19:57:50 -0400

    add caching

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.Router())
-app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname +'/public', { maxAge: 86400000 }));
 
 app.listen(8000);
