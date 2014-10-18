3c996f171c4e9c7b1913b170a5b34e9f1ea89abc
jacksongeller <jacksongeller@gmail.com>
2014-10-17 22:12:23 -0400

    add redirect

--- server.js
+++ server.js
@@ -3,5 +3,12 @@ var app = express();
 
 app.use(express.Router())
 app.use(express.static(__dirname +'/public', { maxAge: 86400000 }));
+app.use(express.Router())
+
+
+app.get('/docs/ventures', function(req, res) {
+  res.redirect('https://docs.google.com/spreadsheets/d/1HKQUpWWYf4FuuV7vSkiXahCUtW8TvWWa3jdk-0m2Rhs/edit?usp=sharing');
+});
+
 
 app.listen(8000);
